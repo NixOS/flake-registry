e32f5376a8d0bd151fbf61b508555705ea446a2e
Dusk Banks <me@bb010g.com>
2023-10-29 16:07:04 -0700

    style: ensure flake registry remains sorted
    Additionally, ensure the latest contents of `flake-registry.json` are
    used when running `ci.sh`. Without `--tarball-ttl 0`, old contents can
    be reused across multiple runs. This shouldn't be an issue in CI, but
    it's helpful when running `ci.sh` manually, during development.

--- ci.sh
+++ ci.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-# Test that the flake-registry.json format is valid
+# Test that the flake-registry.json format is sorted & valid
 set -euo pipefail
 
 cd "$(dirname "$0")"
+
+# Ensure current flake-registry.json file is sorted.
+nix registry list --tarball-ttl 0 --flake-registry "$PWD/flake-registry.json" \
+  | grep -- '^global ' | LC_ALL=C sort -u -c
 
 nix run --flake-registry "$PWD/flake-registry.json" nixpkgs#hello
